4c108581f5e68128ea0b539e1c5ef9d897717350
ankitasharma1 <ankita_sharma1@brown.edu>
2020-04-04 00:57:47 -0400

    more updates

--- src/frontend/main.py
+++ src/frontend/main.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 app = Flask('frontend', static_url_path='', static_folder='static', template_folder='templates')
 
 HOSPITAL_NAME = os.environ.get('HOSPITAL_NAME', 'hospital_name')
+API_GATEWAY_PORT = os.environ.get('API_GATEWAY_PORT', 'api_gateway_port')
 
 
 @app.route('/', methods=['GET'])
@@ -16,11 +17,19 @@ def home():
 
 @app.route('/patient', methods=['GET'])
 def patient():
-    return render_template('patient.html')
+    data = {
+        'hospital_name': HOSPITAL_NAME,
+        'api_gateway_port': API_GATEWAY_PORT
+    }
+    return render_template('patient.html', **data)
 
 @app.route('/physician', methods=['GET'])
 def physician():
-    return render_template('physician.html')
+    data = {
+        'hospital_name': HOSPITAL_NAME,
+        'api_gateway_port': API_GATEWAY_PORT
+    }    
+    return render_template('physician.html', **data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
