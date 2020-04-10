2b2138270cabf1d46f5d6a7dd21e47e93156691f
Irvin Lim <irvinlimweiquan@gmail.com>
2020-04-11 07:39:16 +0800

    Removed unused Python sagas implementation

--- src/consistent_storage/sagas/partitions.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-
-def get_partition(key: str, partitions: int) -> int:
-    h = hashlib.sha256()
-    h.update(key.encode('utf-8'))
-    h_int = int.from_bytes(h.digest(), "big")
-    return h_int % partitions
