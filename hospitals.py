6e0240f66c0abd2a828ee5adfc31cd6775424847
Irvin Lim <irvinlimweiquan@gmail.com>
2020-04-13 16:38:16 +0800

    Add moar features to hospitals.py startup script

--- hospitals.py
+++ hospitals.py
@@ -2,16 +2,31 @@
 Helper script that starts Docker Compose namespaces as separate hospitals.
 """
 
+from __future__ import print_function
+
 import os
 import re
 import sys
-from subprocess import Popen, PIPE
+from subprocess import Popen
 
 BASE_FRONTEND_PORT = 8000
 BASE_API_GATEWAY_PORT = 8100
 
 DOCKER_COMPOSE_FILE = 'docker-compose.hospital.yml'
 HOSPITAL_NAMES_FILE = 'data/hospitals.txt'
+
+PROXIED_COMMANDS = {
+    'logs': 'Display logs for containers in each hospital',
+    'ps': 'List containers',
+    'down': 'Stop and remove containers, networks, images, and volumes (if flags are specified)',
+}
+
+TERMCOLOR = {
+    'ok': '\033[92m[^]\033[0m',
+    'info': '\033[94m[*]\033[0m',
+    'warn': '\033[93m[!]\033[0m',
+    'error': '\033[91m[#]\033[0m',
+}
 
 
 def get_docker_compose_executable():
@@ -38,9 +53,16 @@ def dco(project_name, command_args, env=None):
     args += command_args
     command = ' '.join(args)
 
+    print(TERMCOLOR['info'], 'Executing:', command)
+
     env = os.environ.update(env)
     proc = Popen(args, stdout=sys.stdout, stderr=sys.stderr, env=env, bufsize=1, universal_newlines=True)
-    proc.communicate()
+
+    try:
+        proc.communicate()
+    except KeyboardInterrupt:
+        print(TERMCOLOR['error'], 'Aborting command.')
+        return
 
     if proc.returncode != 0:
         lines = []
@@ -73,9 +95,26 @@ def execute(args):
             dco(slug, args, env=env)
 
 
-if __name__ == "__main__":
+def print_help():
+    def print_cmd_help(cmd, desc):
+        print('  %s\t\t%s' % (cmd, desc))
+
+    print('This script runs Docker Compose commands for each hospital as specified in data/hospitals.txt.')
+    print()
+    print('Usage: python hospitals.py COMMAND [OPTIONS]')
+    print()
+    print('Commands:')
+    print_cmd_help('start', 'Builds and starts services')
+    print_cmd_help('stop', 'Stop services')
+    for cmd, desc in PROXIED_COMMANDS.items():
+        print_cmd_help(cmd, desc)
+    print_cmd_help('help', 'Display this help message')
+
+
+def main():
+
     if len(sys.argv) < 2:
-        print('Usage: python hospitals.py [start|stop] [OPTIONS]')
+        print_help()
         sys.exit(1)
 
     command = sys.argv[1]
@@ -86,9 +125,16 @@ if __name__ == "__main__":
     elif command == 'stop':
         execute(['stop'])
 
-    elif command == 'down':
-        execute(['down'])
+    elif command in PROXIED_COMMANDS:
+        execute(sys.argv[1:])
+
+    elif command == 'help':
+        print_help()
 
     else:
-        print('Usage: python hospitals.py [start|stop] [OPTIONS]')
+        print_help()
         sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
