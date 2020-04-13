196ea8a298da6eeba1e09cb9809158be1fb10fa8
Irvin Lim <irvinlimweiquan@gmail.com>
2020-04-13 18:23:36 +0800

    Fix lint error

--- src/lib/crypto.py
+++ src/lib/crypto.py
@@ -7,5 +7,5 @@ from Crypto.PublicKey import RSA
 def encrypt(data, pub_key):
     rsa = RSA.importKey(pub_key)
     cipher = PKCS1_v1_5.new(rsa)
-    cipherText = cipher.encrypt(data.encode('utf-8'))
-    return base64.b64encode(cipherText).decode('utf-8')
+    cipher_text = cipher.encrypt(data.encode('utf-8'))
+    return base64.b64encode(cipher_text).decode('utf-8')
