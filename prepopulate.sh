5b73ca150db7a32e4ad6954d8d7a826bae8f3155
James Rolfe <jamesnrolfe@gmail.com>
2020-05-07 00:55:59 -0400

    a lot of bobs

---
+++ prepopulate.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+mkdnet() {
+  if [[ ! $(docker network ls --filter "name=^$1$" --quiet) ]]
+  then
+    docker network create "$1"
+  fi
+}
+
+ensure_up() {
+  yaml_name=`echo "$1" | sed 's/docker-compose.\(.*\).yml/\1/'`
+  project_name="cs2952f-$yaml_name"
+  echo Starting "$project_name"...
+
+  files="-f docker-compose.$yaml_name.yml"
+  if [[ -f "docker-compose.$yaml_name.override.yml" ]]; then
+    files="$files -f docker-compose.$yaml_name.override.yml"
+  fi
+
+  dco="docker-compose -p "$project_name" $files"
+
+  $dco up -d --build --remove-orphans
+  stopped=$($dco ps --services --filter status=stopped)
+  if [[ "$stopped" ]]
+  then
+    echo "ERROR: One or more services could not be started: $(echo $stopped | tr '\n' ',' | sed 's/,$/\n/')"
+    echo $stopped | xargs $dco logs --tail=50
+    exit 1
+  fi
+}
+
+# Create Docker networks for inter-hospital communication
+mkdnet hrms-interhospital
+mkdnet hrms-hospital-sagas
+mkdnet hrms-hospital-bigchain
+mkdnet hrms-hospital-centraldb
+mkdnet hrms-hospital-discovery
+
+# Start common infra with Docker Compose
+ensure_up docker-compose.discovery.yml
+ensure_up docker-compose.sagas.yml
+ensure_up docker-compose.bigchain.yml
+ensure_up docker-compose.centraldb.yml
+
+# For testing pruposes
+mkdnet hrms-loadtesting
+ensure_up docker-compose.loadtesting.yml
---
+++ src/prepopulate.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+from sys import exit
+from os import getenv
+import traceback
+import csv
+
+from bigchain_backend import BigchaindbBackend
+from bigchaindb_driver.crypto import generate_keypair
+
+PATH_TO_VOLUME = "/usr/src/app/bigchaindb_client/patient_cards/"
+
+
+def catch_exc(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except:
+            tb = traceback.format_exc()
+            _debug_print(tb)
+            exit(1)
+
+    return wrapper
+
+
+@catch_exc
+def prepopulate():
+    bdb_root_url = getenv('BIGCHAIN_ROOT_URL')
+    if not bdb_root_url:
+        raise Exception('BIGCHAIN_ROOT_URL not set')
+
+    temp_backend = BigchaindbBackend(bdb_root_url)
+
+    for i in range(100):
+        uuid = "bob" + str(i)
+        keys = generate_keypair()
+        temp_backend.put(uuid, keys.public_key)
+
+        _debug_print(f"{uuid} with pub_key:{keys.public_key} put to bigchaindb")
+
+        file_name = PATH_TO_VOLUME + uuid + "_patient_card.csv"
+        with open(file_name, 'w') as f:
+            writer = csv.writer(f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.write_row(['UID', 'PRIV_KEY'])
+            writer.write_row([uuid, keys.private_key])
+
+        _debug_print(f"{uuid} with priv_key:{keys.public_key} written to csv")
+
+
+def _debug_print(msg: str) -> None:
+    print(f"[prepopulate.py] {msg}", flush=True)
+
+
+if __name__ == '__main__':
+    prepopulate()
